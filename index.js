ccb56459a42458d4f88f18e08995b0c0031b8600
--local <--local>
2022-09-25 17:20:36 -0500

    SintCast #30 con xxmotitaxx

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const restart = () => {
 
 var questions = [];
 const request = async () => {
-  let data = await fetch("./presets/all.txt").then((x) => x.text());
+  let data = await fetch("./presets/xxmotitaxx.txt").then((x) => x.text());
   questions = filter(data.split("\n"));
   for (let elem of document.getElementsByClassName("questions-count")) {
     elem.innerHTML = questions.length + " Questions";
